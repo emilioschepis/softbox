fc611d10288ce9824c3e33be8f2af28bb59b3485
Emilio Schepis <emilio.schepis@gmail.com>
2021-12-29 13:22:45 +0100

    initial commit

---
+++ Softbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Softbox
+//
+//  Created by Emilio Schepis on 29/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Softbox/SoftboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftboxApp.swift
+//  Softbox
+//
+//  Created by Emilio Schepis on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SoftboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
